faf83f051fda1a34b6bd15569d5db1dd51c8381b
EinfacheSache <klugetommy@web.de>
2023-07-21 02:42:40 +0200

    Code -> Total code reformation

--- src/main/java/de/cubeattack/neoprotect/bungee/NeoProtectBungee.java
+++ src/main/java/de/cubeattack/neoprotect/bungee/NeoProtectBungee.java
@@ -45,7 +45,8 @@ public final class NeoProtectBungee extends Plugin implements NeoProtectPlugin {
     public void sendMessage(Object receiver, String text, String clickAction, String clickMsg, String hoverAction, String hoverMsg) {
         TextComponent msg = new TextComponent(core.getPrefix() + text);
 
-        if (clickAction != null) msg.setClickEvent(new ClickEvent(ClickEvent.Action.valueOf(clickAction), clickMsg));
+        if (clickAction != null)
+            msg.setClickEvent(new ClickEvent(ClickEvent.Action.valueOf(clickAction), clickMsg));
         if (hoverAction != null)
             msg.setHoverEvent(new HoverEvent(HoverEvent.Action.valueOf(hoverAction), Collections.singletonList(new Text(hoverMsg))));
         if (receiver instanceof CommandSender) ((CommandSender) receiver).sendMessage(msg);
