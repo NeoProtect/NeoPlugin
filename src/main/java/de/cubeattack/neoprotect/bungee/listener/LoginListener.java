35aa6808778d4b5c5445b529d75b226710c9e3c9
EinfacheSache <klugetommy@web.de>
2023-09-02 11:27:37 +0200

    Bugfix -> Fixed message bug

--- src/main/java/de/cubeattack/neoprotect/bungee/listener/LoginListener.java
+++ src/main/java/de/cubeattack/neoprotect/bungee/listener/LoginListener.java
@@ -42,7 +42,7 @@ public class LoginListener implements Listener {
 
                 VersionUtils.Result result = instance.getCore().getVersionResult();
                 if (result.getVersionStatus().equals(VersionUtils.VersionStatus.OUTDATED)) {
-                    instance.sendMessage(player, localization.get(locale, result.getCurrentVersion(), result.getLatestVersion()));
+                    instance.sendMessage(player, localization.get(locale, "plugin.outdated.message", result.getCurrentVersion(), result.getLatestVersion()));
                     instance.sendMessage(player, MessageFormat.format("§7-> §b{0}",
                                     result.getReleaseUrl().replace("/NeoPlugin", "").replace("/releases/tag", "")),
                             "OPEN_URL", result.getReleaseUrl(), null, null);
